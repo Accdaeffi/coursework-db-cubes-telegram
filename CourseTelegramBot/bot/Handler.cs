7ed227a2ad4cf538252de7644950621205ac69a4
Accdaeffi <noskoff2@yandex.ru>
2022-02-23 23:08:06 +0300

    Add project files.

---
+++ CourseTelegramBot/bot/Handler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace CourseTelegramBot.bot
+{
+    class Handler
+    {
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var handler = update.Type switch
+            {
+                // UpdateType.Unknown:
+                // UpdateType.ChannelPost:
+                // UpdateType.EditedChannelPost:
+                // UpdateType.ShippingQuery:
+                // UpdateType.PreCheckoutQuery:
+                // UpdateType.Poll:
+                UpdateType.Message => BotOnMessageReceived(botClient, update.Message!),
+                UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage!),
+                UpdateType.CallbackQuery => BotOnCallbackQueryReceived(botClient, update.CallbackQuery!),
+                UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery!),
+                UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult!),
+                _ => UnknownUpdateHandlerAsync(botClient, update)
+            };
+
+            try
+            {
+                await handler;
+            }
+            catch (Exception exception)
+            {
+                await HandleErrorAsync(botClient, exception, cancellationToken);
+            }
+        }
+    }
+}
---
+++ CourseTelegramBot/cubeConnection/Connection.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AnalysisServices.AdomdClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_telegram_bot.cubeConnection
+{
+    class Connection
+    {
+        private static Connection singleton = null;
+
+        private static readonly string connectionString = "Data Source=localhost;Catalog=course_work";
+        private readonly AdomdConnection connection = new AdomdConnection(connectionString);
+        private readonly CubeDef cube = null;
+
+
+        private Connection()
+        {
+            connection.Open();
+            cube = connection.Cubes[0];
+        }
+
+        public static Connection getConnection()
+        {
+
+            if (singleton == null)
+            {
+                singleton = new Connection();
+            }
+
+            return singleton;
+        }
+
+
+        public CubeDef getCube()
+        {
+            return cube;
+        }
+
+        public void getAllInforamtion() {
+            foreach (CubeDef cube in connection.Cubes)
+            {
+                if (cube.Type != CubeType.Cube) continue;
+                Console.WriteLine("Cube: " + cube.Name);
+
+                foreach (Dimension dim in cube.Dimensions)
+                {
+                    if (dim.Name == "Measures") continue;
+                    Console.WriteLine("  Dimension: " + dim.Name);
+
+                    foreach (Hierarchy hie in dim.Hierarchies)
+                    {
+                        Console.WriteLine("\tHierarchy: " + hie.Name);
+                    }
+                }
+
+                Console.WriteLine("  Measures");
+                foreach (Measure measure in cube.Measures)
+                {
+                    Console.WriteLine("\tFact: " + measure);
+                }
+            }
+        }
+    }
+}
