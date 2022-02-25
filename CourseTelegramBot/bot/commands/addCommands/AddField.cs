08b5a23d017a36c7f6a94f217d01664e14ce2727
Accdaeffi <noskoff2@yandex.ru>
2022-02-25 16:28:22 +0300

    added addfield and addwherepart

---
+++ CourseTelegramBot/bot/commands/addCommands/AddField.cs
@@ -0,0 +1,46 @@
+﻿using CourseTelegramBot.bot.responses;
+using CourseTelegramBot.query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTelegramBot.bot.commands.addCommands
+{
+    class AddField : AbstractCommand
+    {
+        String field;
+
+        public AddField(String field, long userId)
+        {
+            this.field = field;
+            this.userId = userId;
+        }
+
+        public override StringResponse execute()
+        {
+            String responseString;
+
+            QueryConstructor info = MapUserInformation.FindValue(userId);
+
+            if (info == null)
+            {
+                responseString = "Сначала создайте запрос командой /create !";
+            }
+            else
+            {
+                if (info.AddNewMeasure(field))
+                {
+                    responseString = "Успешно добавлено!";
+                }
+                else
+                {
+                    responseString = "Данное поле уже присутствует!";
+                }
+            }
+
+            return new StringResponse(responseString);
+        }
+    }
+}
---
+++ CourseTelegramBot/bot/commands/addCommands/AddWherePart.cs
@@ -0,0 +1,46 @@
+﻿using CourseTelegramBot.bot.responses;
+using CourseTelegramBot.query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTelegramBot.bot.commands.addCommands
+{
+    class AddWherePart : AbstractCommand
+    {
+        String wherePart;
+
+        public AddWherePart(String wherePart, long userId)
+        {
+            this.wherePart = wherePart;
+            this.userId = userId;
+        }
+
+        public override StringResponse execute()
+        {
+            String responseString;
+
+            QueryConstructor info = MapUserInformation.FindValue(userId);
+
+            if (info == null)
+            {
+                responseString = "Сначала создайте запрос командой /create !";
+            }
+            else
+            {
+                if (info.AddNewMeasure(wherePart))
+                {
+                    responseString = "Успешно добавлено!";
+                }
+                else
+                {
+                    responseString = "Данная мера уже присутствует!";
+                }
+            }
+
+            return new StringResponse(responseString);
+        }
+    }
+}
