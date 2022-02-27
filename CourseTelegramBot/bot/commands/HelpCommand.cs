242e177c86c8348f38597f7ad9620ecf808bd241
Accdaeffi <noskoff2@yandex.ru>
2022-02-27 18:03:48 +0300

    added help command

---
+++ CourseTelegramBot/bot/commands/HelpCommand.cs
@@ -0,0 +1,19 @@
+﻿using CourseTelegramBot.bot.responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTelegramBot.bot.commands
+{
+    class HelpCommand : AbstractCommand
+    {
+        readonly String HELP_STRING = "https://telegra.ph/help-dlya-CourseworkDbCubeBot-02-27";
+
+        public override AbstractResponse<string> execute()
+        {
+            return new StringResponse<String>(HELP_STRING);
+        }
+    }
+}
